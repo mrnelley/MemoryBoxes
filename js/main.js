1748615302385dc8178038ca83e9ddcfa03e3777
Mr.Nelley <kelleysoftware@gmail.com>
2017-03-07 09:54:44 -0500

    site updates

--- js/main.js
+++ js/main.js
@@ -30,6 +30,32 @@
   //   proximity: 110
   // });
 
+  var $animation_elements = $('.animation-element');
+  var $window = $(window);
+
+  function check_if_in_view() {
+    var window_height = $window.height();
+    var window_top_position = $window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
+
+    $.each($animation_elements, function() {
+      var $element = $(this);
+      var element_height = $element.outerHeight();
+      var element_top_position = $element.offset().top;
+      var element_bottom_position = (element_top_position + element_height);
+
+      //check to see if this current container is within viewport
+      if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) {
+        $element.addClass('in-view');
+      } else {
+        $element.removeClass('in-view');
+      }
+    });
+  }
+
+  $window.on('scroll resize', check_if_in_view);
+  $window.trigger('scroll');
 
   /*-----------------------------------------------------*/
   /* tabs
@@ -43,7 +69,7 @@
     $(this).addClass("active");
     $(".tab-content").hide();
     var activeTab = $(this).attr("data-id");
-    $("#" + activeTab).fadeIn(700);
+    $("#" + activeTab).fadeIn(900);
   });
 
 
@@ -183,6 +209,7 @@
     /*=====================================================================
     Image loading
     ======================================*/
+
 
     // $(".item-image").delay(1000).animate({ opacity: 1 }, 700);​
 
